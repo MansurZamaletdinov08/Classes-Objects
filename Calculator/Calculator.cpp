8716f532ddca8d203a9276d59437601d9ef3ad50
Mansur Zamaletdinov <zamaletdinov98@list.ru>
2022-11-04 00:15:45 +0300

    My Homeork

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+
+class Calculator {
+    double num1;
+    double num2;
+
+public:
+    double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    double subtract_2_1(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    double subtract_1_2(double num1, double num2) {
+        return num2 - num1;
+    }
+
+    double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    double divide_1_2(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    double divide_2_1(double num1, double num2) {
+        return num2 / num1;
+    }
+
+    bool set_num1(double num1) {
+        this->num1 = num1;
+        if (num1 == 0) {
+            std::cout << "Неверный ввод!" << std::endl;
+            return 1;
+        }
+        else {
+            return num1;
+        }
+
+        return num1;
+    }
+    
+    bool set_num2(double num2) {
+        this->num2 = num2;
+        if (num2 == 0) {
+            std::cout << "Неверный ввод!" << std::endl;
+            return 1;
+        }
+        else {
+            return 1;
+        }
+
+        return num1;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+    double num1, num2;
+
+    int i = 0;
+
+    Calculator operation;
+    while (true) {
+        std::cout << "Введите num1: ";
+        std::cin >> num1;
+
+        std::cout << "Введите num2: ";
+        std::cin >> num2;
+
+        operation.set_num1(num1);
+        operation.set_num2(num2);
+        if (num1 == 0) {
+            std::cout << "Введите ещё раз: ";
+            std::cin >> num1;
+        }
+        if (num2 == 0) {
+            std::cout << "Введите ещё раз: ";
+            std::cin >> num2;
+        }
+        std::cout << "num1 + num2 = " << operation.add(num1, num2) << std::endl;
+        std::cout << "num1 - num2 = " << operation.subtract_2_1(num1, num2) << std::endl;
+        std::cout << "num2 - num1 = " << operation.subtract_1_2(num1, num2) << std::endl;
+        std::cout << "num1 * num2 = " << operation.multiply(num1, num2) << std::endl;
+        std::cout << "num1 / num2 = " << operation.divide_1_2(num1, num2) << std::endl;
+        std::cout << "num2 / num1 = " << operation.divide_2_1(num1, num2) << std::endl;
+    }
+}
---
+++ Counter/Counter.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+
+class Counter {
+public:
+    int y;
+    int incr() {
+        y++;
+        return y;
+    }
+
+    int decr() {
+        y--;
+        return y;
+    }
+
+    int rev() {
+        return y;
+    }
+
+private:
+    int counter = 1;
+};
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+    Counter obj;
+    char f;
+    int x;
+    char op;
+
+    std::cout << "Вы хотите указать начальное значение счётчика ? Введите Y или N : ";
+    std::cin >> f;
+
+    if (f == 'Y') {
+        std::cout << "Введите начальное значение счётчика : ";
+        std::cin >> x;
+    }
+    else {
+        x = 1;
+    }
+
+    obj.y = x;
+
+    do {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> op;
+
+        if (op == '+') {
+            obj.incr();
+        }
+
+        if (op == '-') {
+            obj.decr();
+        }
+
+        if (op == '=') {
+            obj.rev();
+            std::cout << obj.y << std::endl;
+        }
+        if (op == 'x') {
+            std::cout << "До всидания!";
+        }
+    } while (op != 'x');
+}
